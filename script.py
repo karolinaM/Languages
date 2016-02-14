24284e416db5cc38190fd9648b1be17d43ea4eb9
karolinaM <karolina.majstrovic@gmail.com>
2016-02-14 16:40:08 +0100

    script modified for limited repeat

--- script.py
+++ script.py
@@ -8,20 +8,26 @@ import os
 # @param filename File name with all words. First column is word in foreign language and second column in known language (English)
 # @param dictionary Dictionary withwords in foreign language (value), known language (key value)
 def TestFirstColumn(filename, dictionary):
+    again = {}
     with open(filename, 'rb') as csvfile:
         alldata = csv.reader(csvfile, delimiter=';')
         for row in alldata:
             dictionary[row[1]] = row[0]
-    QuestionMe(dictionary)
+    again = QuestionMe(dictionary)
+    while len(again) != 0:
+        again = QuestionMe(again)
 
 # @description Shows dictionary key value and waits for the input and shows if the input is correct.
 # @param dictionary Dictionary withwords in foreign language, known language
 def QuestionMe(dictionary):
-    while True:
-        for word in dictionary.keys():
-            checkThis = raw_input(str(word + ": "))
-            print str(word + ": " + dictionary[word] + " --> " + str(checkThis==dictionary[word]))
-        print "End of file"
+    again = {}
+    for word in dictionary.keys():
+        checkThis = raw_input(str(word + ": "))
+        print str(word + ": " + dictionary[word] + " --> " + str(checkThis==dictionary[word]))
+        if checkThis.lower() != dictionary[word].lower():
+            again[word] = dictionary[word]
+    print "End of dictionary [" + str(len(again)) + " / " + str(len(dictionary)) + "]"
+    return again
 
 def main():
     dictionary = {}
