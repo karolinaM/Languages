9601b5c21685e21addb322dc7e774c1c5779437a
karolinaM <karolina.majstrovic@gmail.com>
2016-02-14 11:33:14 +0100

    Added script with base functionality and file with words

--- README.txt
+++ README.txt
@@ -1,1 +1,6 @@
 Program for easier learning foreign languages
+
+As a command line first parameter is passed filename. File contains words written in Foreign language and in Known language. Languages are separated by character ';'. In this example, Swedish words are in first column and english ones in second column.
+
+Example how to call the script:
+python script.py swedish.txt
---
+++ script.py
@@ -0,0 +1,32 @@
+import sys
+import csv
+
+
+# @TODO: Add random function, so the input is not always the same
+
+# @description Creates dictionary data structure.
+# @param filename File name with all words. First column is word in foreign language and second column in known language (English)
+# @param dictionary Dictionary withwords in foreign language (value), known language (key value)
+def TestFirstColumn(filename, dictionary):
+    with open(filename, 'rb') as csvfile:
+        alldata = csv.reader(csvfile, delimiter=';')
+        for row in alldata:
+            dictionary[row[1]] = row[0]
+    QuestionMe(dictionary)
+
+# @description Shows dictionary key value and waits for the input and shows if the input is correct.
+# @param dictionary Dictionary withwords in foreign language, known language
+def QuestionMe(dictionary):
+    while True:
+        for word in dictionary.keys():
+            checkThis = raw_input(str(word + ": "))
+            print str(word + ": " + dictionary[word] + " --> " + str(checkThis==dictionary[word]))
+        print "End of file"
+
+def main():
+    dictionary = {}
+    TestFirstColumn(str(sys.argv[1]), dictionary)
+
+
+if __name__ == '__main__':
+    main()
